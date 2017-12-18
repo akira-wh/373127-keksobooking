dfe8f28eb4112fc3c24b8880859b897f93a41d2a
Akira <154.4k1r4@gmail.com>
2017-12-18 07:52:16 +0600

    4.38 Теперь ВСЕ модули обёрнуты в IIFE.

--- js/constants.js
+++ js/constants.js
@@ -10,99 +10,102 @@
 ***********************************************************************************
 */
 
-window.constants = {
+(function () {
 
-  // Коды клавиш
-  ESC_KEYCODE: 27,
+  window.constants = {
 
-  // Смещение всех пинов (кроме управляющего) по X и Y (в px)
-  PIN_SHIFT_X: 5,
-  PIN_SHIFT_Y: 37,
+    // Коды клавиш
+    ESC_KEYCODE: 27,
 
-  // Карта пинов и объявлений
-  MAP: document.querySelector('.map'),
+    // Смещение всех пинов (кроме управляющего) по X и Y (в px)
+    PIN_SHIFT_X: 5,
+    PIN_SHIFT_Y: 37,
 
-  // Контейнер пинов
-  PINS_CONTAINER: document.querySelector('.map__pins'),
+    // Карта пинов и объявлений
+    MAP: document.querySelector('.map'),
 
-  // Управляющий пользовательский пин
-  CONTROL_PIN: document.querySelector('.map__pin--main'),
+    // Контейнер пинов
+    PINS_CONTAINER: document.querySelector('.map__pins'),
 
-  // Смещение управляющего пина по Y (в px)
-  CONTROL_PIN_SHIFT_Y: 45,
+    // Управляющий пользовательский пин
+    CONTROL_PIN: document.querySelector('.map__pin--main'),
 
-  // Ограничения координат управляющего пина
-  COORDS_MIN_LIMIT_X: 0,
-  COORDS_MAX_LIMIT_X: 1200,
-  COORDS_MIN_LIMIT_Y: 55,
-  COORDS_MAX_LIMIT_Y: 455,
+    // Смещение управляющего пина по Y (в px)
+    CONTROL_PIN_SHIFT_Y: 45,
 
-  // Форма создания объявлений
-  FORM: document.querySelector('.notice__form'),
+    // Ограничения координат управляющего пина
+    COORDS_MIN_LIMIT_X: 0,
+    COORDS_MAX_LIMIT_X: 1200,
+    COORDS_MIN_LIMIT_Y: 55,
+    COORDS_MAX_LIMIT_Y: 455,
 
-  // Form Action Url
-  FORM_ACTION_URL: 'https://js.dump.academy/keksobooking',
+    // Форма создания объявлений
+    FORM: document.querySelector('.notice__form'),
 
-  // Массив — Заголовки объявлений
-  OFFERS_TITLES: [
-    'Большая уютная квартира',
-    'Маленькая неуютная квартира',
-    'Огромный прекрасный дворец',
-    'Маленький ужасный дворец',
-    'Красивый гостевой домик',
-    'Некрасивый негостеприимный домик',
-    'Уютное бунгало далеко от моря',
-    'Неуютное бунгало по колено в воде'
-  ],
+    // Form Action Url
+    FORM_ACTION_URL: 'https://js.dump.academy/keksobooking',
 
-  // Объект — Типы жилья (ключи и расшифровки)
-  OFFERS_PROPERTY_TYPES: {
-    flat: 'Квартира',
-    house: 'Дом',
-    bungalo: 'Бунгало'
-  },
+    // Массив — Заголовки объявлений
+    OFFERS_TITLES: [
+      'Большая уютная квартира',
+      'Маленькая неуютная квартира',
+      'Огромный прекрасный дворец',
+      'Маленький ужасный дворец',
+      'Красивый гостевой домик',
+      'Некрасивый негостеприимный домик',
+      'Уютное бунгало далеко от моря',
+      'Неуютное бунгало по колено в воде'
+    ],
 
-  // Массив — Время checkin и checkout
-  OFFERS_TIMES: [
-    '12:00',
-    '13:00',
-    '14:00'
-  ],
-
-  // Массив — Преимущества жилья
-  OFFERS_FEATURES: [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner'
-  ],
-
-  // Библиотека ошибок валидации формы
-  INPUT_ERRORS: {
-    valueMissing: 'Это поле не должно быть пустым.',
-    valueShort: 'Минимально допустимая длина: 30 символов. Сейчас: ',
-    valueLong: 'Максимально допустимая длина: 100 символов. Сейчас: ',
-    rangeUnderflow: 'Минимально допустимое значение: ',
-    rangeOverflow: 'Максимально допустимое значение: ',
-    badInput: 'Неверный формат ввода: допустимы только числа.',
-
-    // Выдача динамически составленной ошибки "Значение слишком короткое"
-    getValueShortDynamicError: function (currentLength) {
-      return this.valueShort + currentLength + '.';
+    // Объект — Типы жилья (ключи и расшифровки)
+    OFFERS_PROPERTY_TYPES: {
+      flat: 'Квартира',
+      house: 'Дом',
+      bungalo: 'Бунгало'
     },
-    // Выдача динамически составленной ошибки "Значение слишком длинное"
-    getValueLongDynamicError: function (currentLength) {
-      return this.valueLong + currentLength + '.';
-    },
-    // Выдача динамически составленной ошибки "Число меньше допустимого минимума"
-    getRangeUnderflowDynamicError: function (currentLimit) {
-      return this.rangeUnderflow + currentLimit + '.';
-    },
-    // Выдача динамически составленной ошибки "Число больше допустимого максимума"
-    getRangeOverflowDynamicError: function (currentLimit) {
-      return this.rangeOverflow + currentLimit + '.';
+
+    // Массив — Время checkin и checkout
+    OFFERS_TIMES: [
+      '12:00',
+      '13:00',
+      '14:00'
+    ],
+
+    // Массив — Преимущества жилья
+    OFFERS_FEATURES: [
+      'wifi',
+      'dishwasher',
+      'parking',
+      'washer',
+      'elevator',
+      'conditioner'
+    ],
+
+    // Библиотека ошибок валидации формы
+    INPUT_ERRORS: {
+      valueMissing: 'Это поле не должно быть пустым.',
+      valueShort: 'Минимально допустимая длина: 30 символов. Сейчас: ',
+      valueLong: 'Максимально допустимая длина: 100 символов. Сейчас: ',
+      rangeUnderflow: 'Минимально допустимое значение: ',
+      rangeOverflow: 'Максимально допустимое значение: ',
+      badInput: 'Неверный формат ввода: допустимы только числа.',
+
+      // Выдача динамически составленной ошибки "Значение слишком короткое"
+      getValueShortDynamicError: function (currentLength) {
+        return this.valueShort + currentLength + '.';
+      },
+      // Выдача динамически составленной ошибки "Значение слишком длинное"
+      getValueLongDynamicError: function (currentLength) {
+        return this.valueLong + currentLength + '.';
+      },
+      // Выдача динамически составленной ошибки "Число меньше допустимого минимума"
+      getRangeUnderflowDynamicError: function (currentLimit) {
+        return this.rangeUnderflow + currentLimit + '.';
+      },
+      // Выдача динамически составленной ошибки "Число больше допустимого максимума"
+      getRangeOverflowDynamicError: function (currentLimit) {
+        return this.rangeOverflow + currentLimit + '.';
+      }
     }
-  }
-};
+  };
+})();
