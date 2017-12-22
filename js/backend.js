c5fce68c64283098c56e9ca6ce35f960db3c9332
Akira <154.4k1r4@gmail.com>
2017-12-22 14:08:43 +0600

    5.16 Дублирование настроек для backend.save/load вынесено в функцию.

--- js/backend.js
+++ js/backend.js
@@ -23,20 +23,7 @@
      * @param {callback} onError — callback, обрабатывающий ошибки
      */
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.timeout = window.constants.HTTP_TIMEOUT_LIMIT;
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === window.constants.HTTP_STATUS_OK) {
-          onLoad(xhr.response);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError(xhr.status);
-      });
-
+      var xhr = getBasicTunedXhr(onLoad, onError);
       xhr.open('GET', window.constants.SERVER_DOWNLOAD_URL, true);
       xhr.send();
     },
@@ -50,22 +37,37 @@
      * @param {callback} onError — callback, обрабатывающий ошибки
      */
     save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.timeout = window.constants.HTTP_TIMEOUT_LIMIT;
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === window.constants.HTTP_STATUS_OK) {
-          onLoad(xhr.response);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError(xhr.status);
-      });
-
+      var xhr = getBasicTunedXhr(onLoad, onError);
       xhr.open('POST', window.constants.SERVER_UPLOAD_URL, true);
       xhr.send(data);
     }
   };
+
+  /**
+  * Базовая настройка XMLHttpRequest.
+  * Подходит для получения данных и отправки.
+  * Не содержит конечных команд OPEN() и SEND().
+  *
+  * @function getBasicTunedXhr
+  * @param {callback} onLoad — callback, обрабатывающий данные
+  * @param {callback} onError — callback, обрабатывающий ошибки
+  * @return {object} — настроенный XMLHttpRequest объект
+  */
+  function getBasicTunedXhr(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = window.constants.HTTP_TIMEOUT_LIMIT;
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === window.constants.HTTP_STATUS_OK) {
+        onLoad(xhr.response);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError(xhr.status);
+    });
+
+    return xhr;
+  }
 })();
