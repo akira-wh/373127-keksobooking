cfd56c3f54a5ce1566123a782e52abd0ff153ed9
Akira <154.4k1r4@gmail.com>
2017-12-24 18:06:55 +0600

    6.03 Количество отрисовываемых пинов выражено константой (Д19).

--- js/map.js
+++ js/map.js
@@ -64,7 +64,7 @@
       window.data[i]['data-serial'] = i;
     }
 
-    window.showPins(5, window.data);
+    window.showPins(window.constants.PINS_MAX_LIMIT, window.data);
     window.filters.activate();
   }
 
